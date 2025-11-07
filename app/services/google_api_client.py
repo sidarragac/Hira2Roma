d47d1fdc51ca29cf0e37ccde897d547a1d72fa96
Santiago Idarraga Ceballos <san200526@gmail.com>
2025-11-06 20:39:06 -0500

    Implement Google API client for text translation

--- app/services/google_api_client.py
+++ app/services/google_api_client.py
@@ -1,2 +1,18 @@
+import os
+
+from google.cloud import translate_v2 as translate
+from config.config import Config
+
 class GoogleAPIClient:
-    pass
+    def __init__(self):
+        self.client = self.create_connection()
+    
+    def create_connection(self):
+        folder = Config.KEYS_FOLDER
+        key_path = os.path.join(folder, Config.GCP_KEY_FILE_NAME)
+        client = translate.Client.from_service_account_json(key_path)
+        return client
+
+    def translate_text(self, text):
+        result = self.client.translate(text, target_language='es')
+        return result['translatedText']
